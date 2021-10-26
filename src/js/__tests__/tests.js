5d503f6b4d3d4058b38ab6b76511e3abe3351721
Konstantin Beloglazov <ajoq@ya.ru>
2021-10-26 12:16:27 +0300

    tests v2

--- src/js/__tests__/tests.js
+++ src/js/__tests__/tests.js
@@ -3,22 +3,11 @@ import fetchData from '../http';
 
 jest.mock('../http');
 
-test('request', () => {
-  fetchData.mockReturnValueOnce({ status: 'ok', level: 10 });
-
-  getLevel(1);
-
-  expect(fetchData).toBeCalledWith('https://server/user/1');
-});
-
-test('response status ok', () => {
-  fetchData.mockReturnValueOnce({ status: 'ok', level: 10 });
-
-  expect(getLevel(1)).toEqual('Ваш текущий уровень: 10');
-});
-
-test('response status bad', () => {
-  fetchData.mockReturnValueOnce({ status: 'Bad Request', level: 10 });
-
-  expect(getLevel(1)).toEqual('Информация об уровне временно недоступна');
+test.each([
+  [{ status: 'ok', level: 10 }, 'Ваш текущий уровень: 10'],
+  [{ status: 'Bad Request', level: 10 }, 'Информация об уровне временно недоступна'],
+])('response', (response, expected) => {
+  fetchData.mockReturnValue(response);
+  const result = getLevel(1);
+  expect(result).toEqual(expected);
 });
